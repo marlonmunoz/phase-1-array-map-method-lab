69b2793a09f5b4d1a55f89a75a375aa9e6dd08c8
Marlon Munoz <marlon@marlonshade.com>
2024-07-21 16:19:26 -0400

    array lab

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// function capFirtChar(array){
+//   const titleCased = tutorials.map(word => word.split(' ').map(char => char.charAt(0).toUpperCase() + char.slice(1)).join(' '));
+//   return titleCased
+// }
+
+// console.log(capFirtChar(tutorials))
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const titleCaseWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return titleCaseWords.join(" ");
+  });
+};
+
+console.log(titleCased());
+
+
+
